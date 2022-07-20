e3c8f366ac7b3e0864135a805af568c5304d00b6
rajkumaryadav199 <kuraj10101999@gmail.com>
2022-07-20 20:47:26 +0530

    made some change

--- lately_clone/src/App.js
+++ lately_clone/src/App.js
@@ -1,10 +1,12 @@
+
 import './App.css';
-import Payment from './pages/Payment/Payment';
+
 function App() {
   return (
     <div className="App">
-       <Payment/>
+       
     </div>
   );
 }
+
 export default App;
