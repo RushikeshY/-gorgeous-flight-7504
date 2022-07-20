829b0967462d63ce3ab57fa8d8be6f76cb70ff4f
rajkumaryadav199 <kuraj10101999@gmail.com>
2022-07-20 21:10:52 +0530

    payment page is added

---
+++ lately_clone/src/pages/Payment/Payment.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Container ,Box, Input, Flex} from '@chakra-ui/react'
+import "./Payment.css"
+const Payment = () => {
+  return (
+    <Container className='fw188-Container'>
+    
+       <Box>
+        <h2>Select Your Lately Plan</h2>
+        <p  >The world's most human A.I. is waiting for you - start your free <span style={{fontSize:"20px" }}>7-day trial</span> today!</p>
+      </Box>
+       <Box>
+        <h2>1. Payment Information</h2>
+        <Box>
+        <p>Credit or Debit Card*</p>
+        <input type="number" placeholder='Card number' />
+        <input type="month" placeholder='' />
+        </Box>
+    <Box>
+<Flex >
+<label>Name on Card*</label>
+        <br/>
+        <Input
+    isInvalid
+    errorBorderColor='red.300'
+    focusBorderColor='pink.400'
+    placeholder=''
+  />
+   <br/>
+   <label>Name on Card*</label>
+        <br/>
+        <Input
+    isInvalid
+    errorBorderColor='red.300'
+    focusBorderColor='pink.400'
+    placeholder=''
+  />
+   <br/>
+   <label>Name on Card*</label>
+        <br/>
+        <Input
+    isInvalid
+    errorBorderColor='red.300'
+    focusBorderColor='pink.400'
+    placeholder=''
+  />
+        
+</Flex>
+</Box>  
+     </Box>
+       <Box></Box>
+       <Box></Box>
+       <Box></Box>
+    </Container>
+  )
+}
+
+export default Payment
