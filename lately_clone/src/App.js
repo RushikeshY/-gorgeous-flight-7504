698aa547f4d5c9aac211a440d3145732588ae310
Bharani619 <bharanish619@gmail.com>
2022-07-21 20:30:46 +0530

    Day-2 task completed

--- lately_clone/src/App.js
+++ lately_clone/src/App.js
@@ -8,9 +8,7 @@ function App() {
   return (
     <div className="App">
        <Mainroutes/>
-       <Footer/>
        <Product/>
-
     </div>
   );
 }
--- lately_clone/src/pages/Mainroutes.jsx
+++ lately_clone/src/pages/Mainroutes.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import { Home } from './Home'
 
 const Mainroutes = () => {
   return (
+    <>
+    <div><Navbar/></div>
     <Routes>
        <Route path='/' element={<Home/>}/>
     </Routes>
     <div>
-     
+      <Footer/>
     </div>
-
+    </>
   )
 }
 
