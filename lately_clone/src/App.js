f50726cb8a71c186f0909342c0d3f0a4f4daeba9
RushikeshY <99939510+RushikeshY@users.noreply.github.com>
2022-07-21 18:09:30 +0530

    Product_page_Day2_ Done

--- lately_clone/src/App.js
+++ lately_clone/src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 import Footer from './components/Footer';
 import Mainroutes from './pages/Mainroutes';
+import ContentWriter from './pages/Product/ContentWriter';
 import Product from './pages/Product/Product'
 
 function App() {
   return (
     <div className="App">
-       <Mainroutes/>
-       <Footer/>
+       {/* <Mainroutes/> */}
+      
        <Product/>
+       <Footer/>
+       {/* <ContentWriter/> */}
 
     </div>
   );
